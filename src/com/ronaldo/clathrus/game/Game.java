017c7eaec60119f647a962ecfb1e7bd55239d6bb
Ronaldo Vieira <ronaldoesv@gmail.com>
2016-04-02 17:11:21 -0300

    Episode 2: Threads

--- src/com/ronaldo/clathrus/game/Game.java
+++ src/com/ronaldo/clathrus/game/Game.java
@@ -1,7 +1,28 @@
 package com.ronaldo.clathrus.game;
 
-public class Game {
+public class Game implements Runnable {
 	public static int width = 300;
 	public static int height = (width / 16) * 9;
 	public static int scale = 3;
+	
+	private Thread thread;
+	
+	public synchronized void start() {
+		thread = new Thread(this, "Display");
+		thread.start();
+	}
+	
+	public synchronized void stop() {
+		try {
+			thread.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
 }

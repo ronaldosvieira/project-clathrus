2255157994110f499038fdbc3192052da42597a7
Ronaldo Vieira <ronaldoesv@gmail.com>
2016-04-02 17:35:20 -0300

    Episode 4: Window

--- src/com/ronaldo/clathrus/game/Game.java
+++ src/com/ronaldo/clathrus/game/Game.java
@@ -1,12 +1,27 @@
 package com.ronaldo.clathrus.game;
 
-public class Game implements Runnable {
+import java.awt.Canvas;
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+public class Game extends Canvas implements Runnable {
+	private static final long serialVersionUID = 1L;
+	
 	public static int width = 300;
 	public static int height = (width / 16) * 9;
 	public static int scale = 3;
 	
 	private Thread thread;
+	private JFrame frame;
 	private boolean running = false;
+	
+	public Game() {
+		Dimension size = new Dimension(width * scale, height * scale);
+		setPreferredSize(size);
+		
+		frame = new JFrame();
+	}
 	
 	public synchronized void start() {
 		running = true;
@@ -27,7 +42,20 @@ public class Game implements Runnable {
 	@Override
 	public void run() {
 		while (running) {
-			
+			System.out.println("Running...");
 		}
+	}
+	
+	public static void main(String[] args) {
+		Game game = new Game();
+		game.frame.setResizable(false);
+		game.frame.setTitle("Project Clathrus");
+		game.frame.add(game);
+		game.frame.pack();
+		game.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		game.frame.setLocationRelativeTo(null);
+		game.frame.setVisible(true);
+		
+		game.start();
 	}
 }

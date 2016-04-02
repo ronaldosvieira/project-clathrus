25de582c94546b4e875cfc5f77004c13dadcb0dc
Ronaldo Vieira <ronaldoesv@gmail.com>
2016-04-02 17:17:11 -0300

    Episode 3: Game Loop

--- src/com/ronaldo/clathrus/game/Game.java
+++ src/com/ronaldo/clathrus/game/Game.java
@@ -6,13 +6,17 @@ public class Game implements Runnable {
 	public static int scale = 3;
 	
 	private Thread thread;
+	private boolean running = false;
 	
 	public synchronized void start() {
+		running = true;
 		thread = new Thread(this, "Display");
 		thread.start();
 	}
 	
 	public synchronized void stop() {
+		running = false;
+		
 		try {
 			thread.join();
 		} catch (InterruptedException e) {
@@ -22,7 +26,8 @@ public class Game implements Runnable {
 
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
-		
+		while (running) {
+			
+		}
 	}
 }

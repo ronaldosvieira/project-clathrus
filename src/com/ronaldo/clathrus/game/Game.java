f9517b120d5e0552f4c690968f77f4202ae9b614
Ronaldo Vieira <ronaldoesv@gmail.com>
2016-04-02 18:28:16 -0300

    Episode 7: Buffered Image and Rasters

--- src/com/ronaldo/clathrus/game/Game.java
+++ src/com/ronaldo/clathrus/game/Game.java
@@ -5,6 +5,8 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
 
 import javax.swing.JFrame;
 
@@ -18,6 +20,9 @@ public class Game extends Canvas implements Runnable {
 	private Thread thread;
 	private JFrame frame;
 	private boolean running = false;
+	
+	private BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+	private int[] pixels = ((DataBufferInt) image.getRaster().getDataBuffer()).getData();
 	
 	public Game() {
 		Dimension size = new Dimension(width * scale, height * scale);

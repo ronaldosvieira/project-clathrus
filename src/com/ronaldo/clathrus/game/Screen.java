6b319fcda0b9c1ccedbab09e230b8f659752b67d
Ronaldo Vieira <ronaldoesv@gmail.com>
2016-04-02 19:12:34 -0300

    Episode 9: Rendering Pixels

--- src/com/ronaldo/clathrus/game/Screen.java
+++ src/com/ronaldo/clathrus/game/Screen.java
@@ -9,4 +9,12 @@ public class Screen {
 		this.height = height;
 		this.pixels = new int[width * height];		
 	}
+	
+	public void render() {
+		for (int y = 0; y < height; y++) {
+			for (int x = 0; x < width; x++) {
+				pixels[x + y * width] = 0xff00ff;
+			}
+		}
+	}
 }
--- src/com/ronaldo/clathrus/game/graphics/Game.java
+++ src/com/ronaldo/clathrus/game/graphics/Game.java
@@ -71,10 +71,17 @@ public class Game extends Canvas implements Runnable {
 			return;
 		}
 		
+		screen.render();
+		
+		for (int i = 0; i < pixels.length; i++) {
+			pixels[i] = screen.pixels[i];
+		}
+		
 		Graphics g = bst.getDrawGraphics();
 		
 		g.setColor(Color.BLACK);
 		g.fillRect(0, 0, getWidth(), getHeight());
+		g.drawImage(image, 0, 0, getWidth(), getHeight(), null);
 		
 		g.dispose();
 		bst.show();
